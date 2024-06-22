1e21a2cc04209cf38f3bdcc9a1cfa890d9521a2d
Sergey Tolkachyov <musstudent@mail.ru>
2024-06-22 20:53:57 +0400

    - Изменена вёрстка выбора изображений из уже загруженных при редактировании товара с табличной вёрстки на Bootstrap 5. Классами `row-cols-*` можно настроить адаптив. - добавил картинкам Loading lazy, так как в папке может быть несколько тысяч изображений. Что браузер меньше тупил на этом окне. - строку поиска зафиксировал в sticky-top, чтоб не терялась при прокрутке - удалил неиспользуемые css правила

--- admin/tmpl/productimages/list.php
+++ admin/tmpl/productimages/list.php
@@ -1,51 +1,62 @@
 <?php
+
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
 /**
-* @version      5.0.0 15.09.2018
-* @author       MAXXmarketing GmbH
-* @package      Jshopping
-* @copyright    Copyright (C) 2010 webdesigner-profi.de. All rights reserved.
-* @license      GNU/GPL
-*/
+ * @version      5.0.0 15.09.2018
+ * @author       MAXXmarketing GmbH
+ * @package      Jshopping
+ * @copyright    Copyright (C) 2010 webdesigner-profi.de. All rights reserved.
+ * @license      GNU/GPL
+ */
 defined('_JEXEC') or die();
 ?>
 <form action="index.php?option=com_jshopping&controller=productimages&task=display&tmpl=component" method="post">
-<div class="js-stools clearfix jshop_block_filter">
-    <div class="js-stools-container-bar">
-        <div class="btn-toolbar" role="toolbar">
-
-            <div class="btn-group mr-2">
-                <div class="input-group">
-                    <input name="filter" value="<?php echo htmlspecialchars($this->filter);?>" class="form-control" placeholder="<?php print Text::_('JSHOP_SEARCH')?>" type="text">
-                    <span class="input-group-append">
-                        <button type="submit" class="btn btn-primary hasTooltip" title="<?php print Text::_('JSHOP_SEARCH')?>">
+    <div class="js-stools clearfix jshop_block_filter mb-3 sticky-top py-1" style="background-color: var(--body-bg);">
+        <div class="js-stools-container-bar">
+            <div class="btn-toolbar" role="toolbar">
+                <div class="btn-group mr-2">
+                    <div class="input-group">
+                        <input name="filter" value="<?php echo htmlspecialchars($this->filter); ?>" class="form-control"
+                               placeholder="<?php print Text::_('JSHOP_SEARCH') ?>" type="text">
+                        <span class="input-group-append">
+                        <button type="submit" class="btn btn-primary hasTooltip"
+                                title="<?php print Text::_('JSHOP_SEARCH') ?>">
                             <span class="icon-search" aria-hidden="true"></span>
                         </button>
                     </span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="images_list">
-    <?php foreach($this->list as $file){?>
-        <div class="one_image">
-            <table>
-			<tr><td align="center" valign="middle"><div>
-                <a href="#" onclick="parent.jshopAdmin.setImageFromFolder('<?php print $file?>');return false;">
-                    <img title="<?php print $file?>" src="<?php print $this->config->image_product_live_path?>/thumb_<?php print $file?>">
-                </a>
-				</div></td></tr>
-				<tr><td valign="bottom" align="center"><div>
-                <a href="#" onclick="parent.jshopAdmin.setImageFromFolder('<?php print $file?>');return false;">
-                    <?php print $file?>
-                </a>
-			</div></td></tr>
-            </table>
-		</div>
-    <?php }?>
 
-    <div style="clear: both"></div>
-</div>
+
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+		<?php foreach ($this->list as $file): ?>
+            <div class="col">
+                <div class="card h-100">
+					<?php
+					$img_url     = $this->config->image_product_live_path . '/thumb_' . $file;
+					$img_attribs = [
+						'class'   => 'img-fluid',
+						'loading' => 'lazy',
+						'title'   => $file,
+					];
+					echo HTMLHelper::image($img_url, $file, $img_attribs);
+					?>
+                    <div class="card-body">
+						<?php
+						$link_attribs = [
+							'onclick' => 'parent.jshopAdmin.setImageFromFolder(\'' . $file . '\');return false;',
+							'class'   => 'stretched-link'
+						];
+						echo HTMLHelper::link('#', $file, $link_attribs);
+						?>
+                    </div>
+                </div>
+            </div>
+		<?php endforeach; ?>
+    </div>
 </form>
