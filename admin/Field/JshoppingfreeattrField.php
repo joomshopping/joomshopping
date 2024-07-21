277781bab87ac7481b3738ef24fc55f7e1faa63f
Sergey Tolkachyov <musstudent@mail.ru>
2024-07-21 16:15:49 +0400

    - Добавлено поле JForm для свободных атрибутов - добавлено пропущенное объявление массива

--- admin/Field/JshoppingfreeattrField.php
+++ admin/Field/JshoppingfreeattrField.php
@@ -1,36 +1,37 @@
 <?php
 /**
- * @version      5.5.0 07.06.2024
+ * @version      5.5.1
  * @author       MAXXmarketing GmbH
  * @package      Jshopping
  * @copyright    Copyright (C) 2010 webdesigner-profi.de. All rights reserved.
  * @license      GNU/GPL
  */
-
 namespace Joomla\Component\Jshopping\Administrator\Field;
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Form\Field\ListField;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Form\Field\ListField;
 use Joomla\Component\Jshopping\Site\Lib\JSFactory;
 
-class JshoppingextrafieldsField extends ListField
+class JshoppingfreeattrField extends ListField
 {
-	protected $type = 'jshoppingextrafields';
+
+	protected $type = 'jshoppingfreeattr';
 
 	protected function getOptions(): array
 	{
 		require_once(JPATH_SITE . '/components/com_jshopping/bootstrap.php');
-		$productfield = JSFactory::getTable('productfield');
-        $list = $productfield->getList();
 		$options = [];
+		$freeattributes = JSFactory::getTable('freeattribut');
+		$namesfreeattributes = $freeattributes->getAllNames();
+
 		$default = $this->element['default'] ?? null;
-        if (isset($default) && ((string)$default) === '') {
-            $options[] = HTMLHelper::_('select.option', '', '');
-        }
-		foreach ($list as $item) {
-			$options[] = HTMLHelper::_('select.option', $item->id, $item->name);
+		if (isset($default) && ((string)$default) === '') {
+			$options[] = HTMLHelper::_('select.option', '', '');
+		}
+		foreach ($namesfreeattributes as $attr_id => $attr_name) {
+			$options[] = HTMLHelper::_('select.option', $attr_id, $attr_name);
 		}
 
 		return $options;
